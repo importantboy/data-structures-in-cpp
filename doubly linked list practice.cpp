3bb4d1eb2e856706d31017b86e6e7c0c83cffe40
importantboy <57150785+importantboy@users.noreply.github.com>
2024-02-20 00:02:47 +0530

    added insert and delete feature in doubly linked list practice file

--- doubly linked list practice.cpp
+++ doubly linked list practice.cpp
@@ -1,107 +1,146 @@
 #include <iostream>
-#include <string>
-
 using namespace std;
+
 class Node
 {
 public:
-    int key;
-    string value;
+    int value;
     Node *prev;
     Node *next;
 
-    Node(int key, string value)
+    Node(int value)
     {
-        this->key = key;
         this->value = value;
         this->prev = nullptr;
         this->next = nullptr;
+    }
+    ~Node()
+    {
+        if (this->next != nullptr)
+        {
+            delete this->next;
+            this->next = nullptr;
+        }
     }
 };
 
 class DoublyLinkedList
 {
 public:
-    int key;
     Node *head;
     Node *tail;
-    DoublyLinkedList(int key, string value)
-    {
-        this->key = key;
-        Node *newnode = new Node(key, value);
-        this->head = newnode;
-        this->tail = newnode;
-    }
-    void insertNode(int position, int key, string value)
-    {
-        Node *newnode = new Node(key, value);
-        if (position > this->tail->key)
-        {
-            newnode->prev = this->tail;
-            this->tail->next = newnode;
-            this->tail = newnode;
-        }
-        else if (position < this->head->key)
-        {
-            newnode->next = this->head;
-            this->head->prev = newnode;
-            this->head = newnode;
-        }
-        
-    }
-    void insertAtmiddle(int position,int key , string value){
-        int index = 1;
-        Node* newnode = new Node(key , value);
-        Node* current = this->head;
-        while(current != nullptr){
-             if(this->head->prev == nullptr){
-                 cout << "cannot insert position first"<< endl;
-                 break;
-             }
-              else if(this->tail->next == nullptr){
-                cout << "insert at last position " << endl;
-              }
-               else{
-                        
-               }
-              current = current->next;
-        }
-    };
-    void printList()
-    {
-        Node *current = this->head;
 
-        while (current != nullptr)
-        {
-            cout << "[key:" << current->key << " ";
-            cout << "value:" << current->value << "]"
-                 << " ";
-            current = current->next;
-        }
-    };
-    Node *gethead()
+    DoublyLinkedList(int value)
     {
-        return this->head;
+        Node *initialnode = new Node(value);
+        this->head = initialnode;
+        this->tail = initialnode;
+    }
+    protected:
+    void insertAfter(int value)
+    {
+        Node *newnode = new Node(value);
+        this->tail->next = newnode;
+        newnode->prev = this->tail;
+        this->tail = newnode;
+    };
+    protected:
+    void insertBefore(int value)
+    {
+        Node *newnode = new Node(value);
+        this->head->prev = newnode;
+        newnode->next = this->head;
+        this->head = newnode;
+    };
+    public:
+    void insertNode(int position, int value)
+    {
+        if (position == 1)
+        {
+            insertBefore(value);
+            return;
+        }
+        Node *temp = this->head;
+
+        Node *newnode = new Node(value);
+        int count = 1;
+        while (count < position - 1)
+        {
+            temp = temp->next;
+            count++;
+        }
+        if (temp->next == nullptr)
+        {
+            insertAfter(value);
+            return;
+        }
+        newnode->next = temp->next;
+        newnode->prev = temp;
+        temp->next = newnode;
+    }
+    void deleteNode(int position)
+    {
+        if (position == 1)
+        {
+            Node *temp = this->head;
+            this->head = this->head->next;
+            temp->next = nullptr;
+            delete temp;
+        }
+        else
+        {
+            int count = 1;
+            Node *current = this->head;
+            while (count < position)
+            {
+                current = current->next;
+                count++;
+            }
+            if (current->next == nullptr)
+            {
+                this->tail = current->prev;
+                current->prev->next = nullptr;
+                delete current;
+            }
+            else
+            {
+                current->prev->next = current->next;
+                current->next->prev = current->prev;
+                current->next = nullptr;
+                delete current;
+            }
+        }
+    }
+    void Printlist()
+    {
+        Node *temp = this->head;
+        while (temp != nullptr)
+        {
+            cout << temp->value << " ";
+            temp = temp->next;
+        }
+    }
+
+    Node *getHead()
+    {
+        return head;
     }
     Node *getTail()
     {
-        return this->tail;
+        return tail;
     }
 };
 int main()
 {
-    // initialize the list with first node
-    DoublyLinkedList list(1, "first");
-    // insert node;
-    list.insertNode(2, 2, "second");
-    list.insertNode(3, 3, "third");
-    list.insertNode(-1, 0, "zero");
-    list.insertNode(-2, -1, "-one");
-    list.insertAtmiddle(-1,20,"rahul");
-    list.printList();
+    DoublyLinkedList list(1);
+   list.insertNode(2,2);
+   list.insertNode(3,3);
+   list.insertNode(4,11);
+   list.insertNode(5,31);
+   list.insertNode(6,90);
+   list.insertNode(7,87);
+    list.Printlist();
     cout << endl;
-    cout << "the current head is at key : " << list.gethead()->key << endl;
-    cout << "the current tail is at key : " << list.getTail()->key << endl;
-
-    return 0;
-}
+    cout << "the value of the head is :" << list.getHead()->value << endl;
+    cout << "the value of the tail is :" << list.getTail()->value << endl;
+};
